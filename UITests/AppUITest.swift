bb738f5076d6ef364b0596b79deedc40783326ec
Aubrey Gu <1642414548@qq.com>
2023-03-24 15:49:57 +0800

    Adding a new unit and UI test

---
+++ UITests/AppUITest.swift
@@ -0,0 +1,37 @@
+//
+//  AppUITest.swift
+//  FitnessDataAnalysis
+//
+//  Created by 十口V🌼 on 2023/3/24.
+//
+
+//import XCTest
+
+
+//final class UITests: XCTestCase {
+//
+//    var app = XCUIApplication()
+//    override func setUpWithError() throws {
+//      try super.setUpWithError()
+//      continueAfterFailure = false
+//      app = XCUIApplication()
+//      app.launch()
+//    }
+//
+//
+    
+//    func testAddCoffee() throws {
+//      CoffeesScreen()
+//        .tapAddCoffeeButton()
+//    }
+
+//    func testAddCoffeeWithNoNameShowsAlert() {
+//      CoffeesScreen()
+//        .tapAddCoffeeButton()
+//      CoffeeScreen()
+//        .tapSaveCoffee()
+//        .tapCloseSaveCoffeeErrorAlertButton()
+//    }
+    
+    
+//}
---
+++ UITests/Screens.swift
@@ -0,0 +1,13 @@
+//
+//  Screens.swift
+//  FitnessDataAnalysis
+//
+//  Created by 十口V🌼 on 2023/3/24.
+//
+
+//
+//import XCTest
+//
+//class Screen {
+//  var app = XCUIApplication()
+//}

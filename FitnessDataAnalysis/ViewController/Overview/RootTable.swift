b4e2161110091c33a8e874ac50c7300c15a8b371
Aubrey <1642414548@qq.com>
2023-07-07 00:24:58 +0800

    添加了ViewController文件夹说明

--- FitnessDataAnalysis/ViewController/Overview/RootTable.swift
+++ FitnessDataAnalysis/ViewController/Overview/RootTable.swift
@@ -15,11 +15,16 @@ class RootTable: UITableViewController {
     /// 页面最底部的提示信息
     @IBOutlet weak var footLabel: UILabel!
     
+    
     // MARK: - properties
     /// 各个Table的ID
     var cellSecID = ["sec1", "sec2", "sec3"]
     /// 是否可以访问用户健康数据且数据数量不为0
     var dataAuth: Bool = false
+    /// 第二个section的列表内容
+    var sec2Content: [String] = []
+    /// 第二个section项目的logo
+    var sec2LogoName: [String] = []
     
     
     // MARK: - 生命周期
@@ -27,6 +32,8 @@ class RootTable: UITableViewController {
         super.viewDidLoad()
         // 数据权限
         self.testDataAuth()
+        // 加载plist内容
+        self.getPlistContent()
         // 配置
         self.setBasicInfo()
     }
@@ -34,7 +41,9 @@ class RootTable: UITableViewController {
     
     /// 设置基础的UI和配置信息
     func setBasicInfo() {
+        // 高亮颜色
         self.editBtn.tintColor = ColorUtil.getBarBtnColor()
+
     }
     
     /// 尝试是否可以访问用户健康数据
@@ -59,12 +68,14 @@ class RootTable: UITableViewController {
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return dataAuth ? 3 : 1
+        // TODO: 取消注释
+//        return dataAuth ? 3 : 1
+        return 3
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return [1, 6, 2][section]
+        return [1, self.sec2Content.count, 2][section]
     }
 
     override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
@@ -87,14 +98,15 @@ class RootTable: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+
         
         let cell = tableView.dequeueReusableCell(withIdentifier: self.cellSecID[indexPath.section], for: indexPath)
-
         var content = cell.defaultContentConfiguration()
         
         // 未获取数据/数据量为0
-        if !self.dataAuth {
-            if indexPath.row == 0 {
+        // TODO: - 取消注释这个if !self.dataAuth 和else的注释
+//        if !self.dataAuth {
+            if indexPath.row == 0 && indexPath.section == 0 {
                 content.text = NSLocalizedString("noDataHintText", comment: "")
                 content.secondaryText = "🙁"
                 content.secondaryTextProperties.alignment = .center
@@ -104,20 +116,43 @@ class RootTable: UITableViewController {
                 self.editBtn.isHidden = true
             }
 
-        }
+//        }
         // 可以成功访问数据
-        else {
+//        else {
             if indexPath.section == 1 {
-                content.text = "Test"
+                // 显示文本
+                content.text = self.sec2Content[indexPath.row]
+                content.image = UIImage(systemName: self.sec2LogoName[indexPath.row])
+                content.imageProperties.tintColor = UIColor.blue
             }
             
-        }
+//        }
         
         
         cell.contentConfiguration = content
         return cell
     }
 
+    /// 获取overviewItem.plist的内容
+    func getPlistContent() {
+        let plistURL = Bundle.main.url(forResource: "overviewItem", withExtension: "plist")
+        // 设置各个var的值
+        do {
+            let data = try Data(contentsOf: plistURL!)
+            let plistData = try PropertyListSerialization.propertyList(from: data, options: [], format: nil)
+            let dataDictionary = plistData as! [String: Any]
+            // sec2的各个列表名称
+            let sec2List = dataDictionary["sec2Array"]! as! [String]
+            self.sec2Content = sec2List
+            self.sec2LogoName = dataDictionary["sec2LogoName"]! as! [String]
+        }
+        catch{
+            
+        }
+        
+    }
+    
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
--- FitnessDataAnalysis/ViewController/readMe.md
+++ FitnessDataAnalysis/ViewController/readMe.md
@@ -1,12 +1,39 @@
 # ViewController文件说明
 
+> 主要了解“概要”tab栏下的各个子模块。
+
+## 概要（文件夹`Overview`目录）
+
+<img src="img/dic01.png" style="zoom: 50%;" >
+
+`RootTable.swift` 是APP的主界面对应的视图控制器（也是概要Tab的根视图控制器），作用：
+
+* 控制section1、2和3的UI、显示文字和跳转界面
+* 显示section1的具体数据分析结果
+
+---
+
+`overviewItem.plist` 存储需要显示&本地化（翻译）的文本。
+
+---
+
+概要下的各个分区列表项（按钮）对应的代码在文件夹`Overview`目录下。`Overview`目录下的三个子目录：
+
+* `sec1`对应section1。如果授权失败，这个section会用来提示用户无法访问；否则，这个section初步确定功能：最近一次运动的分析概况。并且点击section1跳转到更加具体的界面。
+* `sec2`对应section2。包含的列表项目：
+  * 历史数据查询、可视化
+  * 数据分析，包括异常检测
+  * 
+* `sec3`对应section3。包含的列表项目：
+  * 制定计划功能，与push notification组合
+  * 
+
+<img src="img/overview.png" style="zoom: 25%;" >
 
 
-`RootTable.swift` 是APP的主界面对应的视图控制器，也就是下图中Tab栏的”概要“。
 
-<img src="img/overview.jpeg" style="zoom: 25%;" >
+## 其他（先不用理会）
 
-
-
-
-
+* 设置tab栏在`Settings`文件夹下
+* 实用工具tab栏在`Toolbox`文件夹下
+* 
